668337d872405c9414fe9e280b1c6dfbe7a9fe5a
Hendric Eckelt <Hendric.Eckelt@gmail.com>
2022-07-29 17:23:04 +0000

    Introduce a sidebar for navigation

--- documentation/docs/.vitepress/config.ts
+++ documentation/docs/.vitepress/config.ts
@@ -2,5 +2,15 @@ import {defineConfig} from 'vitepress'
 
 export default defineConfig({
   lang: 'en-US',
+  themeConfig: {
+    sidebar: [
+      {
+        items: [
+          {text: 'Introduction', link: '/introduction/'},
+          {text: 'Getting Started', link: '/introduction/getting-started'},
+        ],
+      },
+    ],
+  },
   title: 'K8S Observability',
 })
